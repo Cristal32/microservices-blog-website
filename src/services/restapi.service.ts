00cd1b728d90ffeaa67566237b503a43be5c8431
Your Name <you@example.com>
2024-05-05 01:55:00 +0200

    update api

--- src/services/restapi.service.ts
+++ src/services/restapi.service.ts
@@ -17,44 +17,43 @@ export class RestapiService {
   constructor(private http: HttpClient,private zone:NgZone) { }
   public getProfile(id:number):Observable<user>{
    
-    return this.http.get<user>(`http://localhost:9098/user/profile/${id}`)
+    return this.http.get<user>(`http://localhost:8222/users/profile/${id}`)
 }
 
  public editProfile(u: user,id:number) {
 
 
-
-  return this.http.put<void>(`http://localhost:9098/user/edit/${id}`, u);
+  return this.http.put<user>(`http://localhost:8222/users/edit/${id}`, u);
 }
 public addUser(u:user){
-  return this.http.post<user>(`http://localhost:9098/user/add`, u);
+  return this.http.post<user>(`http://localhost:8222/users/add`, u);
 }
 public authentifier(email:string,pass:string){
-  return this.http.get<user>(`http://localhost:9098/user/authentifier/${email}/${pass}`);
+  return this.http.get<user>(`http://localhost:8222/users/authentifier/${email}/${pass}`);
 }
 
 public addBlog(blog: FormData, id: number ): Observable<any> {
 
-  return this.http.post<Blog>(`http://localhost:9097/blogs/addBlog/${id}`, blog);
+  return this.http.post<Blog>(`http://localhost:8222/blogs/addBlog/${id}`, blog);
 }
 
 public getBlogsByUserId(userId: number): Observable<Blog[]> {
-  return this.http.get<Blog[]>(`http://localhost:9097/blogs/user/${userId}`);
+  return this.http.get<Blog[]>(`http://localhost:8222/blogs/user/${userId}`);
 }
 
 public getAllBlogs(): Observable<BlogResponse[]> {
-  return this.http.get<BlogResponse[]>(`http://localhost:9097/blogs/all`);
+  return this.http.get<BlogResponse[]>(`http://localhost:8222/blogs/all`);
 }
 
 public getBlogsByCountry(country: string): Observable<BlogResponse[]> {
   // Use template literals to include the country parameter in the URL
-  return this.http.get<BlogResponse[]>(`http://localhost:9097/blogs/country/${country}`);
+  return this.http.get<BlogResponse[]>(`http://localhost:8222/blogs/country/${country}`);
 }
 
 
 
 getBlogById(blogid: number): Observable<Blog> {
-  return this.http.get<Blog>(`http://localhost:9097/blogs/content/${blogid}`);
+  return this.http.get<Blog>(`http://localhost:8222/blogs/content/${blogid}`);
 }
 
 }
